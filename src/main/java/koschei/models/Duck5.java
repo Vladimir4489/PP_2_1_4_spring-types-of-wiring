8062cf0f0a38178c61023e531437bbaaaf4580c8
Vladimir4489 <kostroma-gold@mail.ru>
2022-08-11 14:31:03 +0300

    Commit 1

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,24 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    private Egg6 egg;
+//    @Autowired
+//    public void setEgg(Egg6 egg) {
+//        this.egg = egg;
+//    }
+
+    @Autowired
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
